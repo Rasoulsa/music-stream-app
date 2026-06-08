44e23826d7dd9edbf240f94bb1b78e6b7748bb55
Rasoul Saeidi <saeidirasoul@gmail.com>
2026-06-08 13:13:23 +0330

    feat: add search, filtering, and ordering to song api

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -45,6 +45,7 @@ THIRD_PARTY_APPS = [
     "rest_framework",
     "corsheaders",
     "drf_spectacular",
+    "django_filters",
 ]
 
 LOCAL_APPS = [
@@ -147,6 +148,11 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": ("rest_framework.pagination.PageNumberPagination"),
     "PAGE_SIZE": 20,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
 }
 
 # -----------------------------------------------------------------------------
